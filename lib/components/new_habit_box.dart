fdbf129b0f983ea3f433736a49a4f8aca6226228
Bawbits <mohdkabiru.shek5@gmail.com>
2023-10-25 18:46:44 +0100

    Progress

---
+++ lib/components/new_habit_box.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class EnterNewHabitBox extends StatelessWidget {
+  final controller;
+  final VoidCallback onSave;
+  final VoidCallback onCancel;
+  const EnterNewHabitBox(
+      {super.key,
+      required this.controller,
+      required this.onCancel,
+      required this.onSave});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Colors.grey[900],
+      content: TextField(
+        controller: controller,
+        style: const TextStyle(color: Colors.white),
+        decoration: const InputDecoration(
+          enabledBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+        ),
+      ),
+      actions: [
+        MaterialButton(
+            onPressed: onSave,
+            child: Text(
+              "Save",
+              style: TextStyle(color: Colors.white),
+            ),
+            color: Colors.black),
+        MaterialButton(
+            onPressed: onCancel,
+            child: Text('Cancel', style: TextStyle(color: Colors.white)),
+            color: Colors.black)
+      ],
+    );
+  }
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:activity_tracker/components/habit_tile.dart';
+import 'package:activity_tracker/components/new_habit_box.dart';
+import 'components/my_fab.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List todayHabitList = [
+    ["Morning Run", false],
+    ["Evening Game", false],
+  ];
+
+  void checkBoxTapped(bool? value, int index) {
+    setState(() {
+      todayHabitList[index][1] = value;
+    });
+  }
+
+  final _newHabitNameController = TextEditingController();
+  void createNewHabit() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return EnterNewHabitBox(
+          controller: _newHabitNameController,
+          onSave: saveNewHabit,
+          onCancel: cancelNewHabit,
+        );
+      },
+    );
+  }
+
+  void saveNewHabit() {
+    setState(() {
+      todayHabitList.add([_newHabitNameController.text, false]);
+    });
+    _newHabitNameController.clear();
+
+    Navigator.of(context).pop();
+  }
+
+  void cancelNewHabit() {
+    _newHabitNameController.clear();
+    Navigator.of(context).pop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        floatingActionButton: MyFloatingActionButton(
+          onPressed: createNewHabit,
+        ),
+        backgroundColor: Colors.grey[300],
+        body: ListView.builder(
+            itemCount: todayHabitList.length,
+            itemBuilder: ((context, index) {
+              return HabitTile(
+                habitName: todayHabitList[index][0],
+                habitCompleted: todayHabitList[index][1],
+                onChanged: (value) => checkBoxTapped(value, index),
+              );
+            })));
+  }
+}
